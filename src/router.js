f54952775cbeb5ee6ee2212e9f8468b0c8eaea89
jolaya182 <jolaya182@gmail.com>
2021-09-07 15:28:49 -0400

    added throw exception

--- src/router.js
+++ src/router.js
@@ -1,83 +1,78 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
-const model = require('./rhinoceros');
-const {allowSpeciesTable, allowedNameTable} = require('./conts/const');
+const model = require("./rhinoceros");
+const { allowSpeciesTable, allowedNameTable } = require("./conts/const");
 
-router.get('/rhinoceros', async (ctx, next) => {
-  const {id} = ctx.request.query;
-  
+router.get("/rhinoceros", async (ctx, next) => {
+  const { id } = ctx.request.query;
+
   console.log("id", id);
 
   const rhinoceroses = model.getAll();
 
-  if(id){
+  if (id) {
     const rhino = await model.getRhinoById(id);
-    if(rhino){
+    if (rhino) {
       ctx.response.body = { rhino };
       return;
-     }
-    ctx.response.body =  {
+    }
+    ctx.response.body = {
       id: -1,
-      name: 'name not found',
-      species: 'species not found',
+      name: "name not found",
+      species: "species not found",
     };
     return;
   }
   ctx.response.body = { rhinoceroses };
 });
 
-const IsRulePropertyNameSpeciesEnforced = async (rhinoObject)=>{
+const IsRulePropertyNameSpeciesEnforced = async (rhinoObject) => {
   const properties = Object.keys(rhinoObject);
 
   let arePropertiesValid = false;
-  arePropertiesValid = await properties.every((prop)=>allowedNameTable.has(prop))
+  arePropertiesValid = await properties.every((prop) =>
+    allowedNameTable.has(prop)
+  );
   // check if the number of properties are at least the same or higher
-  if(properties.length < allowedNameTable.size) arePropertiesValid = false;
+  if (properties.length < allowedNameTable.size) arePropertiesValid = false;
 
   await console.log("arePropVal", arePropertiesValid);
 
   return arePropertiesValid;
-}
+};
 
-const IsRuleNameSpeciesValuesEnforced =  (rhinoObject)=>{
+const IsRuleNameSpeciesValuesEnforced = (rhinoObject) => {
   // const table set
   // get it from hash table from const file
 
+  let areSpeciesNamesAllowed = false;
 
-  let  areSpeciesNamesAllowed = false;
-
-  if(allowSpeciesTable.has(rhinoObject.species)){
+  if (allowSpeciesTable.has(rhinoObject.species)) {
     areSpeciesNamesAllowed = true;
-    return areSpeciesNamesAllowed
-  };
-
-  return areSpeciesNamesAllowed;
-}
-
-
-router.post('/rhinoceros', async (ctx, next) => {
-  const {body} = ctx.request;
-  const isBodyPropertiesCorrect = await IsRulePropertyNameSpeciesEnforced(body);
-  const isBodyContentCorrect = await IsRuleNameSpeciesValuesEnforced(body);
-  if(!isBodyPropertiesCorrect){
-    //throw error exception
-    const RhinoNotPermitted =  {
-      id: -1,
-      errorMessage: 'properties have to be and written as the following \'name\' and \'species\' ',
-    };
-    ctx.response.body = RhinoNotPermitted;
-    return
+    return areSpeciesNamesAllowed;
   }
 
-  if(!isBodyContentCorrect){
-    //throw error exception
-    const RhinoNotPermitted =  {
-      id: -1,
-      errorMessage: 'species submitted is not within the permissible species, please write another species that is allowed',
+  return areSpeciesNamesAllowed;
+};
 
-    };
-    ctx.response.body = RhinoNotPermitted;
-    return
+router.post("/rhinoceros", async (ctx, next) => {
+  const { body } = ctx.request;
+  const isBodyPropertiesCorrect = await IsRulePropertyNameSpeciesEnforced(body);
+  const isBodyContentCorrect = await IsRuleNameSpeciesValuesEnforced(body);
+  if (!isBodyPropertiesCorrect) {
+    //throw error exception
+    ctx.throw(
+      400,
+      "Rule Property Name Species is not Enforced. Properties have to be and written as the following 'name' and 'species' '"
+    );
+  }
+
+  if (!isBodyContentCorrect) {
+    //throw error exception
+    ctx.throw(
+      400,
+      "Rule Name Species Values is not Enforced. Species submitted is not within the permissible species, please write another species that is allowed"
+    );
   }
 
   ctx.response.body = model.newRhinoceros(ctx.request.body);
